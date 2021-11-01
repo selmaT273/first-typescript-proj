51b2e45f0eeeb794ad37e6f176bdbaae0a2ba09e
Stacey Teltser <stacey.teltser@gmail.com>
2021-11-01 15:45:58 -0500

    added onChange to capture text input and onClick to addTodo

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
@@ -7,15 +7,21 @@ interface Props {
 }
 
 export const TodoForm: React.FC<Props> = ({ addTodo }) => {
+  const [text, setText] = useState('');
   return (
     <>
     <h2>Add a todo item</h2>
     <Form>
       <Form.Group controlId="todoItem">
         <Form.Label>Description</Form.Label>
-        <Form.Control type="text" />
+        <Form.Control type="text" value={text} onChange={e => {setText(e.target.value)}}/>
       </Form.Group>
-      <Button variant="primary" type="submit">Add Todo</Button>
+      <Button variant="primary" type="submit" onClick={e => {
+        e.preventDefault();
+        addTodo(text);
+        setText('')}}>
+          Add Todo
+      </Button>
     </Form>
     </>
   );
