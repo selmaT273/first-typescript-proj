a55a89d1fea73422e857ed5c559151f9ac12c70d
Stacey Teltser <stacey.teltser@gmail.com>
2021-11-01 14:51:57 -0500

    rendering 2 todos from todos array

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import { TodoListItem } from './Components/TodoListItem';
+
+const todos: Todo[] = [
+  {
+    text: 'do laundry',
+    complete: false,
+  },
+  {
+    text: 'grade papers',
+    complete: true,
+  },
+];
 
 function App() {
   return (
     <>
-      Hello World.
+      <TodoListItem todo={todos[0]}/>
+      <TodoListItem todo={todos[1]}/>
     </>
   );
 }
