0e0137e662bda843ed17a0bc9aa12e3ec4005ba1
Stacey Teltser <stacey.teltser@gmail.com>
2021-12-09 16:33:08 -0600

    added tooltip with date created

--- src/Components/TodoListItem.tsx
+++ src/Components/TodoListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card }from 'react-bootstrap';
+import { Button, Toast, OverlayTrigger, Tooltip }from 'react-bootstrap';
 import './TodoListItem.css';
 
 interface Props {
@@ -9,25 +9,37 @@ interface Props {
 }
 
 export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo, removeTodo }) => {
+
+  const dateCreated = todo.createdAt ? new Date(todo.createdAt) : null;
+  const formattedDate = dateCreated ? dateCreated.toLocaleDateString('en-us', {year: 'numeric', month: '2-digit', day:'2-digit'}) : null;
+ 
   return (
     <>
-    <Card className="mb-2">
-      <Card.Body>
-      <input
-        type="checkbox" 
-        checked={ todo.status }
-        onClick={() => {
-          toggleTodo(todo);
-        }}
-      />
-      <p style={{ textDecoration: todo.status ? 'line-through' : undefined }}>
-        { todo.name }
-      </p>
-      <Button className="btn-secondary" type="submit" onClick={() => {
-        removeTodo(todo._id)
-      }}>Delete</Button>
-      </Card.Body>
-    </Card>
+    <OverlayTrigger
+      placement="top-end"
+      delay={{show: 250, hide: 400}}
+      overlay={<Tooltip>Created on: { formattedDate }</Tooltip>}>
+    <Toast className="mb-4" onClose={() => {removeTodo(todo._id)}}>
+      <Toast.Header>
+        <Button 
+          size="sm"
+          variant={ todo.status ? "secondary" : "light"}
+          onClick={() => {
+            toggleTodo(todo);
+          }}>
+            {todo.status ? "Complete" : "Pending" }
+        </Button>
+        <span style={{ textDecoration: todo.status ? 'line-through' : undefined }}>
+          { todo.name }
+        </span>
+      </Toast.Header>
+      <Toast.Body>
+        <p style={{ textDecoration: todo.status ? 'line-through' : undefined }}>
+          { todo.description }
+        </p>
+      </Toast.Body>
+    </Toast>
+    </OverlayTrigger>
     </>
   )
 }
